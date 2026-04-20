e06fb9b31ad2ab6ea40c42752eb07a00d5c63c1f
unknown <kud1n.yegorik@gmail.com>
2026-04-20 16:26:03 +0300

    v2 tests add

---
+++ internal/handlers/order/orders_mock.go
@@ -0,0 +1,50 @@
+package order
+
+import (
+	"context"
+	"go-pet-shop/internal/models"
+)
+
+type OrdersMock struct {
+	CreateOrderFunc            func(ctx context.Context, order models.Order) (int, error)
+	AddOrderItemFunc           func(ctx context.Context, orderItem models.OrderItem) error
+	GetOrderByIDFunc           func(ctx context.Context, id int) (models.Order, error)
+	GetOrdersByUserEmailFunc   func(ctx context.Context, email string) ([]models.Order, error)
+	GetOrderItemsByOrderIDFunc func(ctx context.Context, orderID int) ([]models.OrderItem, error)
+}
+
+func (m *OrdersMock) CreateOrder(ctx context.Context, order models.Order) (int, error) {
+	if m.CreateOrderFunc != nil {
+		return m.CreateOrderFunc(ctx, order)
+	}
+
+	return 0, nil
+}
+
+func (m *OrdersMock) AddOrderItem(ctx context.Context, orderItem models.OrderItem) error {
+	if m.AddOrderItemFunc != nil {
+		return m.AddOrderItemFunc(ctx, orderItem)
+	}
+	return nil
+}
+
+func (m *OrdersMock) GetOrderByID(ctx context.Context, id int) (models.Order, error) {
+	if m.GetOrderByIDFunc != nil {
+		return m.GetOrderByIDFunc(ctx, id)
+	}
+	return models.Order{}, nil
+}
+
+func (m *OrdersMock) GetOrdersByUserEmail(ctx context.Context, email string) ([]models.Order, error) {
+	if m.GetOrdersByUserEmailFunc != nil {
+		return m.GetOrdersByUserEmailFunc(ctx, email)
+	}
+	return nil, nil
+}
+
+func (m *OrdersMock) GetOrderItemsByOrderID(ctx context.Context, orderID int) ([]models.OrderItem, error) {
+	if m.GetOrderItemsByOrderIDFunc != nil {
+		return m.GetOrderItemsByOrderIDFunc(ctx, orderID)
+	}
+	return nil, nil
+}
---
+++ internal/handlers/order/orders_test.go
@@ -0,0 +1,292 @@
+package order
+
+import (
+	"context"
+	"errors"
+	"go-pet-shop/internal/models"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi"
+)
+
+func TestCreateOrder(t *testing.T) {
+	mock := &OrdersMock{
+		CreateOrderFunc: func(ctx context.Context, order models.Order) (int, error) {
+			return 1, nil
+		},
+	}
+
+	order := `{
+		"userID": 1,
+		"total_price":0
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/orders", strings.NewReader(order))
+	w := httptest.NewRecorder()
+
+	handler := New(slog.Default(), mock)
+	handler.CreateOrder(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fatalf("expected 200, got %d", w.Code)
+	}
+}
+
+func TestCreateOrder_BadRequest(t *testing.T) {
+	mock := &OrdersMock{
+		CreateOrderFunc: func(ctx context.Context, order models.Order) (int, error) {
+			return 0, errors.New("Empty userID")
+		},
+	}
+
+	order := `{
+		"userID":,
+		"total_price":0
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/orders", strings.NewReader(order))
+	w := httptest.NewRecorder()
+
+	handler := New(slog.Default(), mock)
+	handler.CreateOrder(w, req)
+
+	if w.Code != http.StatusBadRequest {
+		t.Fatalf("expected 400, got %d", w.Code)
+	}
+}
+
+func TestCreateOrder_Error(t *testing.T) {
+	mock := &OrdersMock{
+		CreateOrderFunc: func(ctx context.Context, order models.Order) (int, error) {
+			return 0, errors.New("DB error")
+		},
+	}
+
+	order := `{
+		"userID":1,
+		"total_price":0
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/orders", strings.NewReader(order))
+	w := httptest.NewRecorder()
+
+	handler := New(slog.Default(), mock)
+	handler.CreateOrder(w, req)
+
+	if w.Code != http.StatusInternalServerError {
+		t.Fatalf("expected 500, got %d", w.Code)
+	}
+}
+
+func TestAddOrderItem_Success(t *testing.T) {
+	mock := &OrdersMock{
+		AddOrderItemFunc: func(ctx context.Context, orderItem models.OrderItem) error {
+			return nil
+		},
+	}
+
+	orderItem := `{
+		"product_id": 1,
+		"quantity": 1
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/orders/1/items", strings.NewReader(orderItem))
+	w := httptest.NewRecorder()
+
+	routeContext := chi.NewRouteContext()
+	routeContext.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeContext))
+
+	handler := New(slog.Default(), mock)
+	handler.AddOrderItem(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fatalf("expected 200, got %d", w.Code)
+	}
+}
+
+func TestAddOrderItem_BadRequest(t *testing.T) {
+	mock := &OrdersMock{
+		AddOrderItemFunc: func(ctx context.Context, orderItem models.OrderItem) error {
+			return errors.New("Bad Request")
+		},
+	}
+
+	orderItem := `{
+		"product_id": 1,
+		"quantity": 1
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/orders/1/items", strings.NewReader(orderItem))
+	w := httptest.NewRecorder()
+
+	routeContext := chi.NewRouteContext()
+	routeContext.URLParams.Add("id", "abc")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeContext))
+
+	handler := New(slog.Default(), mock)
+	handler.AddOrderItem(w, req)
+
+	if w.Code != http.StatusBadRequest {
+		t.Fatalf("expected 400, got %d", w.Code)
+	}
+}
+
+func TestAddOrderItem_Error(t *testing.T) {
+	mock := &OrdersMock{
+		AddOrderItemFunc: func(ctx context.Context, orderItem models.OrderItem) error {
+			return errors.New("DB error")
+		},
+	}
+
+	orderItem := `{
+		"product_id": 1,
+		"quantity": 1
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/orders/1/items", strings.NewReader(orderItem))
+	w := httptest.NewRecorder()
+
+	routeContext := chi.NewRouteContext()
+	routeContext.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeContext))
+
+	handler := New(slog.Default(), mock)
+	handler.AddOrderItem(w, req)
+
+	if w.Code != http.StatusInternalServerError {
+		t.Fatalf("expected 500, got %d", w.Code)
+	}
+}
+
+func TestGetOrderByID_Success(t *testing.T) {
+	mock := &OrdersMock{
+		GetOrderByIDFunc: func(ctx context.Context, id int) (models.Order, error) {
+			return models.Order{}, nil
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/orders/1", nil)
+	w := httptest.NewRecorder()
+
+	routerCtx := chi.NewRouteContext()
+	routerCtx.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routerCtx))
+
+	handler := New(slog.Default(), mock)
+	handler.GetOrderByID(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fatalf("expected 200, got %d", w.Code)
+	}
+}
+
+func TestGetOrderByID_BadRequest(t *testing.T) {
+	mock := &OrdersMock{
+		GetOrderByIDFunc: func(ctx context.Context, id int) (models.Order, error) {
+			return models.Order{}, errors.New("Bad Request")
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/orders/abc", nil)
+	w := httptest.NewRecorder()
+
+	routerCtx := chi.NewRouteContext()
+	routerCtx.URLParams.Add("id", "abc")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routerCtx))
+
+	handler := New(slog.Default(), mock)
+	handler.GetOrderByID(w, req)
+
+	if w.Code != http.StatusBadRequest {
+		t.Fatalf("expected 400, got %d", w.Code)
+	}
+}
+
+func TestGetOrderByID_Error(t *testing.T) {
+	mock := &OrdersMock{
+		GetOrderByIDFunc: func(ctx context.Context, id int) (models.Order, error) {
+			return models.Order{}, errors.New("DB error")
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/orders/1", nil)
+	w := httptest.NewRecorder()
+
+	routerCtx := chi.NewRouteContext()
+	routerCtx.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routerCtx))
+
+	handler := New(slog.Default(), mock)
+	handler.GetOrderByID(w, req)
+
+	if w.Code != http.StatusInternalServerError {
+		t.Fatalf("expected 500, got %d", w.Code)
+	}
+}
+
+func TestGetOrdersByUserEmail_Success(t *testing.T) {
+	mock := &OrdersMock{
+		GetOrdersByUserEmailFunc: func(ctx context.Context, email string) ([]models.Order, error) {
+			return []models.Order{
+				{CustomerID: 1, TotalPrice: 100},
+			}, nil
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/users/orders?email=example@mail.net", nil)
+	w := httptest.NewRecorder()
+
+	handler := New(slog.Default(), mock)
+	handler.GetOrdersByUserEmail(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fatalf("expected 200, got %d", w.Code)
+	}
+}
+
+func TestGetOrdersByUserEmail_BadRequest(t *testing.T) {
+	mock := &OrdersMock{
+		GetOrdersByUserEmailFunc: func(ctx context.Context, email string) ([]models.Order, error) {
+			return nil, errors.New("Bad request")
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/users/orders?gmail=example@mail.net", nil)
+	w := httptest.NewRecorder()
+
+	handler := New(slog.Default(), mock)
+	handler.GetOrdersByUserEmail(w, req)
+
+	if w.Code != http.StatusBadRequest {
+		t.Fatalf("expected 400, got %d", w.Code)
+	}
+}
+
+func TestGetOrdersByUserEmail_Error(t *testing.T) {
+	mock := &OrdersMock{
+		GetOrdersByUserEmailFunc: func(ctx context.Context, email string) ([]models.Order, error) {
+			return nil, errors.New("DB error")
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/users/orders?email=example@mail.net", nil)
+	w := httptest.NewRecorder()
+
+	handler := New(slog.Default(), mock)
+	handler.GetOrdersByUserEmail(w, req)
+
+	if w.Code != http.StatusInternalServerError {
+		t.Fatalf("expected 500, got %d", w.Code)
+	}
+}
